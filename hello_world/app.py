849a0b03c4aa1b8a0bfad5248471c9c2f340f890
ryo-murai <murai.ry@gmail.com>
2022-05-17 22:18:38 +0900

    add flake8-docstring and change the doc-style to google

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,3 +1,5 @@
+"""AWS Lambda sample module"""
+
 import json
 import logging
 import os
@@ -19,23 +21,19 @@ table = dynamodb.Table(os.environ["DYNAMODB_TABLE"])
 def lambda_handler(event: APIGatewayProxyEventV2, context: Context) -> APIGatewayProxyResponseV2:
     """Sample pure Lambda function
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    Args:
+        event (APIGatewayProxyEventV2):
+          API Gateway Lambda Proxy Input Format
+          Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format # noqa: E501
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format # noqa: E501
+        context (Context):
+          Lambda Context runtime methods and attributes
+          Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
 
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    Returns:
+        APIGatewayProxyResponseV2:
+          responce
+          https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
     table.put_item(Item={"id": "aaaaa", "item": event["body"]})
     logger.info("successfully put item")
